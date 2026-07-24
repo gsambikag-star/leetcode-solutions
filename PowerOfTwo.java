1711ba9327150060a099d1c6063229f37c5c9cf9
gsambikag-star <gsambikag@gmail.com>
2026-07-24 20:06:17 +0530

    added power of two

---
+++ PowerOfTwo.java
@@ -0,0 +1,19 @@
+public boolean isPowerOfTwo{
+    if(n<1){
+        return false;
+    }
+    else if(n==1){
+        return true;
+    }
+    else{
+        while(n%2==0){
+            n=n/2;
+        }
+        if(n==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
