7efee77733bad01899e08494b5d0c04fcc1a1cf2
gsambikag-star <gsambikag@gmail.com>
2026-07-25 19:28:42 +0530

    added length of last word

---
+++ LengthofLastWord.java
@@ -0,0 +1,13 @@
+public int lengthofLastWord(string s){
+    int count=0;
+    for(int i=s.length()-1;i>=0;i--){
+        char ch=s.charAt(i);
+        if(ch!=' '){
+            count++;
+        }
+        else if(count!=0){
+            break;
+        }
+    }
+    return count;
+}
