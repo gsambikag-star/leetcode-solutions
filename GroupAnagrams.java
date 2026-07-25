a5deef02d860dbf2d952dcc7d420d63defaf23e2
gsambikag-star <gsambikag@gmail.com>
2026-07-25 20:08:03 +0530

    added group anagrams

---
+++ GroupAnagrams.java
@@ -0,0 +1,20 @@
+public List<List<String>> groupanagrams (String strs[]){
+    Map <String,List> map=new HashMap<>();
+    for(String s: strs){
+        int count[]=new int[26];
+        for(char ch:s.toCharArray()){
+            count[ch-'a']++;
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i:count){
+            sb.append('#');
+            sb.append(i);
+        }
+        String key=sb.toString();
+        if(!map.containsKey(key)){
+            map.put(key,new ArrayList<String>());
+        }
+        map.get(key).add(s);
+    }
+    return new ArrayList(map.values());
+}
